ff7fdffcdd8f1b69afba46c3eb6d04556136884f
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-05-17 12:11:16 -0700

    add twilio module custom and theme

---
+++ sites/all/modules/media_recorder/js/media-recorder.js
@@ -0,0 +1,76 @@
+/**
+ * @file
+ * Provides JavaScript additions to the media recorder element.
+ *
+ * This file loads the correct media recorder javascript based on detected
+ * features, such as the MediaRecorder API and Web Audio API. It has a flash
+ * fallback, and uses the file widget for devices that supports nothing.
+ */
+
+(function ($) {
+  'use strict';
+
+  // Normalize features.
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+  window.URL = window.URL || window.webkitURL;
+
+  // Feature detection.
+  var getUserMediaCheck = typeof (navigator.getUserMedia) === 'function';
+  var mediaRecorderCheck = typeof (window.MediaRecorder) === 'function';
+  var webAudioCheck = typeof (window.AudioContext) === 'function';
+  var swfobjectCheck = typeof (window.swfobject) === 'object';
+  var flashVersionCheck = swfobjectCheck ? (swfobject.getFlashPlayerVersion().major >= 10) : false;
+
+  // Set recorder type.
+  var recorderType = false;
+  if (getUserMediaCheck && webAudioCheck && mediaRecorderCheck) {
+    recorderType = 'MediaRecorder';
+  }
+  else if (getUserMediaCheck && webAudioCheck && !mediaRecorderCheck) {
+    recorderType = 'MediaRecorderHTML5';
+  }
+  else if (swfobjectCheck && flashVersionCheck) {
+    recorderType = 'MediaRecorderFlash';
+  }
+
+  // Attach behaviors.
+  Drupal.behaviors.mediaRecorder = {
+    attach: function (context, settings) {
+      if (settings.mediaRecorder && settings.mediaRecorder.elements) {
+        $.each(settings.mediaRecorder.elements, function (key, info) {
+          $('#' + info.id, context).once('media-recorder', function () {
+            var $mediaRecorder = $('#' + info.id);
+            var $mediaRecorderFallback = $('#' + info.id + '-fallback-ajax-wrapper');
+            switch (recorderType) {
+              case 'MediaRecorder':
+                $mediaRecorder.show();
+                $mediaRecorderFallback.remove();
+                new Drupal.MediaRecorder(info.id, info.conf);
+                break;
+              case 'MediaRecorderHTML5':
+                $mediaRecorder.show();
+                $mediaRecorderFallback.remove();
+                // Use the kaltura video recorder if kaltura is enabled, rather
+                // than the HTML5 audio only Recorder.js library.
+                if (info.conf.kaltura) {
+                  new Drupal.MediaRecorderFlash(info.id, info.conf);
+                }
+                else {
+                  new Drupal.MediaRecorderHTML5(info.id, info.conf);
+                }
+                break;
+              case 'MediaRecorderFlash':
+                $mediaRecorder.show();
+                $mediaRecorderFallback.remove();
+                new Drupal.MediaRecorderFlash(info.id, info.conf);
+                break;
+              default:
+                $mediaRecorder.hide();
+            }
+          });
+        });
+      }
+    }
+  };
+})(jQuery);
---
+++ sites/all/themes/mbase/templates/user-login.tpl.php
@@ -0,0 +1,7 @@
+<?php print drupal_render_children($form) ?>
+
+<div class="login-photo">
+    <img src="<?php print base_path() . drupal_get_path('theme', 'mbase')
+   . '/images/login-photo.jpeg'; ?>"
+        alt="Login" title="Login" width='327' height='221' />
+</div>
